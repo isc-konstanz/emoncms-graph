a96dd1b76aaea1aa20960f563abd6b1e15cfbeb9
Adrian Minde <Adrian_Minde@live.de>
2019-02-08 15:42:39 +0100

    Move menu entry to extra dropdown

--- graph_menu.php
+++ graph_menu.php
@@ -2,5 +2,10 @@
     $domain = "messages";
     bindtextdomain($domain, "Modules/graph/locale");
     bind_textdomain_codeset($domain, 'UTF-8');
-    $menu_dropdown_config[] = array('name'=> dgettext($domain,"Graphs"), 'icon'=>'icon-retweet', 'path'=>"graph" , 'session'=>"write", 'order' => 25 );
-
+    $menu_dropdown[] = array(
+        'name'=> dgettext($domain,"Graphs"),
+        'icon'=>'icon-list-alt',
+        'path'=>"graph",
+        'session'=>"read",
+        'order' => 20
+    );
